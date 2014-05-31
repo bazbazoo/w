6aff801e61a54789e0daba92cea1b1aa03a3aa6b
Itay Donanhirsh <idonenhirsch@twitter.com>
2014-05-31 00:05:45 -0400

    read

--- w/nodes/forbidden.py
+++ w/nodes/forbidden.py
@@ -2,10 +2,13 @@ from web import forbidden
 
 from node import Node
 
+def forbidden_response():
+  forbidden()
+  return "forbidden"
+
 class ForbiddenNode(Node):
   def __call__(self, method):
-    forbidden()
-    return "forbidden"
+    forbidden_response()
 
   def resolve(self, path):
     return self
--- w/nodes/static.py
+++ w/nodes/static.py
@@ -1,8 +1,19 @@
+from web import forbidden, internalerror
+
 from file import FileNode
+from forbidden import forbidden_response
 
 class StaticNode(FileNode):
   def __str__(self):
     return FileNode.__str__(self) + " (static)"
 
   def _GET(self):
-    return str(self)
+    if self.readable:
+      try:
+        with open(self.path, 'rb') as f:
+          i = iter(lambda: f.read(1024), '')
+          for b in i: yield b
+      except IOError as e:
+        internalerror()
+    else:
+      forbidden_response()
